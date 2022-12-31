443a47634796f0dd408aec42ac2d0a71e32868d5
Your <you@example.com>
2022-12-31 13:34:59 +0530

    added contant list items

---
+++ utils/Constant.ts
@@ -0,0 +1,35 @@
+export const profileTypeList = [
+    { name: "profile", entity: "person" },
+    { name: "business", entity: "business" },
+];
+
+export const emotionList = [
+    { name: "None" },
+    { name: "informative" },
+    { name: "fun" },
+    { name: "angry" },
+    { name: "happy" },
+    { name: "funny" },
+    { name: "hungry" },
+    { name: "love" },
+    { name: "loving" },
+    { name: "convincing" },
+    { name: "convince" },
+    { name: "cool" },
+    { name: "controversial" },
+    { name: "serious" },
+    { name: "sad" },
+    { name: "cry" },
+    { name: "curious" },
+    { name: "cute" },
+    { name: "ecstatic" },
+];
+
+export const defaultFormData = {
+    prompt: "",
+    length: "short",
+    tone: "",
+    type: "",
+    hashtags: "0",
+    entity: "person",
+};
