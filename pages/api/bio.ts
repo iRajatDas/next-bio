b144e071217091fa916c06555db10abd1d915029
Your <you@example.com>
2023-01-02 16:21:08 +0530

    removed unused methods

--- pages/api/bio.ts
+++ pages/api/bio.ts
@@ -2,15 +2,22 @@ import NextCors from 'nextjs-cors';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const allowedMethods = ['POST'];
+
 async function handler(req: NextApiRequest, res: NextApiResponse<unknown>) {
   // Run the cors middleware
   // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
   await NextCors(req, res, {
     // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
+    methods: ['POST'],
+    origin: ["https://next-bio.vercel.app", 'http://localhost:3000'],
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
+
+
+  if (!allowedMethods.includes(req.method!) || req.method == 'OPTIONS') {
+    return res.status(405).send({ message: "Unauthorized access" });
+  }
 
   try {
     fetch("https://pallyy.com/api/bios", {
